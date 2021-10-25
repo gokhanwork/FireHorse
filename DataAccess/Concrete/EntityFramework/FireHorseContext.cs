6b15f2c1b618f972a1f160de761ebdefd4ce0e0e
Gökhan <gokhanplt.com@gmail.com>
2021-10-26 02:20:45 +0300

    DbContext Added

---
+++ DataAccess/Concrete/EntityFramework/FireHorseContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Core.Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class FireHorseContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=DESKTOP-QCVLLUQ\SQL2019;Database=FireHorse;Trusted_Connection=true");
+        }
+        public DbSet<OperationClaim> OperationClaims { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<UserOperationClaim> UserOperationClaims { get; set; }
+
+    }
+}
