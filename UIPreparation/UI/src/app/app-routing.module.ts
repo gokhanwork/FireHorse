d2c76ada0578eedbcccc65d890cff295596876a5
Gökhan <gokhanplt.com@gmail.com>
2021-11-02 03:50:14 +0300

    UI Added

--- UIPreparation/UI/src/app/app-routing.module.ts
+++ UIPreparation/UI/src/app/app-routing.module.ts
@@ -1,10 +1,34 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { AuthGuard } from './modules/auth/services/auth.guard';
+import { MyComponent } from './pages/my/my.component';
 
-const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'auth',
+    loadChildren: () =>
+      import('./modules/auth/auth.module').then((m) => m.AuthModule),
+  },
+  {
+    path: 'error',
+    loadChildren: () =>
+      import('./modules/errors/errors.module').then((m) => m.ErrorsModule),
+  },
+  {
+    path: '',
+    canActivate: [AuthGuard],
+    loadChildren: () =>
+      import('./_metronic/layout/layout.module').then((m) => m.LayoutModule),
+  },
+  {
+    path: 'my-component',
+    component: MyComponent,
+  },
+  { path: '**', redirectTo: 'error/404' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ UIPreparation/UI/src/app/pages/deneme/deneme.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-deneme',
+  templateUrl: './deneme.component.html',
+  styleUrls: ['./deneme.component.scss']
+})
+export class DenemeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
